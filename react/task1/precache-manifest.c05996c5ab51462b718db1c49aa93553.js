3241bceccb09135ad5f1db144f9738a6ef35915b
Daniil Chernyshev <des1nteresado@outlook.com>
2019-06-04 16:07:09 +0300

    1

---
+++ react/task1/precache-manifest.c05996c5ab51462b718db1c49aa93553.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77c1fb814f1de96d0304062ee092cbe2",
+    "url": "/react/task1/index.html"
+  },
+  {
+    "revision": "b2e52c331f3cd3bf3521",
+    "url": "/react/task1/static/js/2.7d0aaaa4.chunk.js"
+  },
+  {
+    "revision": "5e6034642bd1eaa03d2e",
+    "url": "/react/task1/static/js/main.b0c510c7.chunk.js"
+  },
+  {
+    "revision": "4c6e6ba5f1ef0b3766dd",
+    "url": "/react/task1/static/js/runtime~main.f378dc76.js"
+  }
+]);
