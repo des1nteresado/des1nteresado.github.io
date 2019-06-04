a7ad4520406c8e140bd1262eef83385fd481d78c
Daniil Chernyshev <des1nteresado@outlook.com>
2019-06-04 15:30:17 +0300

    Changed structure, delete scripts

--- react/task1/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import CounterContainer from './components/CounterContainer'
-
-ReactDOM.render(
-    <CounterContainer />,
-    document.getElementById('root')
-);
