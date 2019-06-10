8a9edb1d47c1f75dcce8ad785306a3e15e05cb94
Daniil Chernyshev <des1nteresado@outlook.com>
2019-06-10 12:22:16 +0300

    Added react task2

---
+++ react/task2/precache-manifest.62854803f6044459cce2bf6100fa1e01.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ba9cd9ec2fba56ac7b3d46317282f93",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a935fbf9f66ad88479f",
+    "url": "/static/js/2.50ec2631.chunk.js"
+  },
+  {
+    "revision": "595684d297450eb9a4d4",
+    "url": "/static/js/main.d41b9b73.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
