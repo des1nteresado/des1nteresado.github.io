34b9232dd57b9264c75a19b0950a4d70e3b70db7
Daniil Chernyshev <des1nteresado@outlook.com>
2019-06-14 11:03:56 +0300

    Updated exact path task5

---
+++ react/task5/precache-manifest.a48fd4f29c280412bc443ab6a3fc8810.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e785759609fa1d66be15e63a15825b0c",
+    "url": "/react/task5/index.html"
+  },
+  {
+    "revision": "f1de81359ec22b5903dd",
+    "url": "/react/task5/static/js/2.452d4d78.chunk.js"
+  },
+  {
+    "revision": "4dbe433022bea7e271dc",
+    "url": "/react/task5/static/js/main.55f60c97.chunk.js"
+  },
+  {
+    "revision": "8d0e7fd6d0da0044d1aa",
+    "url": "/react/task5/static/js/runtime~main.af68cb2d.js"
+  }
+]);
