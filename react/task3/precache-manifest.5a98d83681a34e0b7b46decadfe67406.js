12e1a6a378a823d38b73c6c8913eda2dc13059fd
Daniil Chernyshev <des1nteresado@outlook.com>
2019-06-15 20:04:09 +0300

    Trigger rebuild

---
+++ react/task3/precache-manifest.5a98d83681a34e0b7b46decadfe67406.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "622af61d4281b2408a497ce234e17b9b",
+    "url": "/react/task3/index.html"
+  },
+  {
+    "revision": "75d68f71825b49a15818",
+    "url": "/react/task3/static/js/2.a2fb2143.chunk.js"
+  },
+  {
+    "revision": "a966859972e776dfcd1e",
+    "url": "/react/task3/static/js/main.fb125d15.chunk.js"
+  },
+  {
+    "revision": "848532bbd6204b90af57",
+    "url": "/react/task3/static/js/runtime~main.0bf8d523.js"
+  }
+]);
