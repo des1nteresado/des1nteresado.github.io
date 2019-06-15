9a366cf29bcb7885d5be32cf9801b4829ae702a3
Daniil Chernyshev <des1nteresado@outlook.com>
2019-06-15 18:24:47 +0300

    Added task6

---
+++ react/task6/precache-manifest.4ff6cdef29c0361796361b149b8ed68e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bad359b407a23989db985a5edfa999ad",
+    "url": "/react/task6/index.html"
+  },
+  {
+    "revision": "0f177f08d464ec1820e1",
+    "url": "/react/task6/static/js/2.ecb70031.chunk.js"
+  },
+  {
+    "revision": "4d9546c8411b69c68b16",
+    "url": "/react/task6/static/js/main.ee7cbd43.chunk.js"
+  },
+  {
+    "revision": "4efb86eb851ad25df74a",
+    "url": "/react/task6/static/js/runtime~main.60475731.js"
+  }
+]);
