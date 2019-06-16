77f1118a44c52adf4442e2d9fb4b057347109fde
Daniil Chernyshev <des1nteresado@outlook.com>
2019-06-16 12:34:49 +0300

    restyled task4

--- react/task4/precache-manifest.e920d2ce19901db555292d6256fffa60.js
+++ react/task4/precache-manifest.e920d2ce19901db555292d6256fffa60.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9d1755bdaacc79810c7c6911352c7891",
+    "revision": "dc4a42d9211e9f7085a053b5f80c6d7e",
     "url": "/react/task4/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react/task4/static/js/2.f4190957.chunk.js"
   },
   {
-    "revision": "a83416256525c85f1593",
-    "url": "/react/task4/static/js/main.bdfa3873.chunk.js"
+    "revision": "0ab1cad4d23a238a0a89",
+    "url": "/react/task4/static/js/main.8ab36638.chunk.js"
   },
   {
     "revision": "bd698175c5973d58abce",
--- react/task4/service-worker.js
+++ react/task4/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react/task4/precache-manifest.ea400be3a20fa723091b3aa2d57bbfea.js"
+  "/react/task4/precache-manifest.e920d2ce19901db555292d6256fffa60.js"
 );
 
 self.addEventListener('message', (event) => {
