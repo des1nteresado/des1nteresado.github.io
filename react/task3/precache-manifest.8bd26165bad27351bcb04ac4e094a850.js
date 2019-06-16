c3cea9bfc9b4b89c0177fd840a1bbec8c1197eb2
Daniil Chernyshev <des1nteresado@outlook.com>
2019-06-16 11:52:05 +0300

    restyled task3

---
+++ react/task3/precache-manifest.8bd26165bad27351bcb04ac4e094a850.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30c4c318eec7e7355f8c22e413249cd4",
+    "url": "/react/task3/index.html"
+  },
+  {
+    "revision": "ea792e89ae98f4fb482b",
+    "url": "/react/task3/static/js/2.f493094f.chunk.js"
+  },
+  {
+    "revision": "c5f30ec4cd5a0aca733e",
+    "url": "/react/task3/static/js/main.3c47edc0.chunk.js"
+  },
+  {
+    "revision": "848532bbd6204b90af57",
+    "url": "/react/task3/static/js/runtime~main.0bf8d523.js"
+  }
+]);
